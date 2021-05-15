6d6c25e1f18ab3a18e9d91c7871a74ef47f88174
Alex Larionov <alxs.larionov@gmail.com>
2021-05-15 22:11:36 +0300

    Changed training launch to Python, for better tqdm

--- train.py
+++ train.py
@@ -1,8 +1,7 @@
-from pytorch_lightning.core.datamodule import LightningDataModule
-from pytorch_lightning.core.lightning import LightningModule
-
+import sys
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.cli import LightningCLI
+import torch
 class CustomLightningCLI(LightningCLI):
     def add_arguments_to_parser(self, parser):
         parser.add_argument('--experiment_name', default='DefaultModel')
@@ -26,14 +25,26 @@ class CustomLightningCLI(LightningCLI):
         self.trainer.callbacks.append(callback)
 
 
-def main(gpus=1):
+def main(config_path=None, gpus=1):
+    # Replacing command line argument for --config flag 
+    # with `config_path` variable
+    if config_path:
+        try:
+            idx = sys.argv.index('--config')
+            if idx + 1 < len(sys.argv):
+                sys.argv[idx + 1] = config_path
+            else:
+                sys.argv.append(config_path)
+        except:
+            sys.argv.extend(['--config', config_path])
+
     cli = CustomLightningCLI(
-        LightningModule,
-        LightningDataModule,
+        pl.LightningModule,
+        pl.LightningDataModule,
         trainer_defaults={'gpus': gpus, 'benchmark': (gpus is not None)},
         seed_everything_default=42,
         subclass_mode_model=True,
         subclass_mode_data=True)
 
 if __name__ == '__main__':
-    main(gpus=None)
+    main(gpus=1 if torch.cuda.is_available() else None)
