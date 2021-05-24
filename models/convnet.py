854657a23489d285fc5f72c5a3675457b6a826fc
Alex Larionov <alxs.larionov@gmail.com>
2021-05-25 00:39:48 +0300

    Update convnet configs

--- models/convnet.py
+++ models/convnet.py
@@ -7,6 +7,7 @@ from torch.optim.optimizer import Optimizer
 import torchmetrics
 import models
 from models.loss_functions import EVM
+from data.transform_1d_2d import *
 
 class ConvNet_regressor(pl.LightningModule):
     def __init__(
@@ -51,18 +52,18 @@ class ConvNet_regressor(pl.LightningModule):
 
 
     def forward(self, x):
-        x = x.permute(2, 1, 0)
+        x = x.permute(2, 0, 1)
         real = x.real
         imag = x.imag
         real, imag = self.net(real, imag)
-        return (real + 1j*imag).permute(2, 1, 0)
+        return (real + 1j*imag).unsqueeze(1).permute(1, 2, 0)
 
 
     def training_step(self, batch, batch_idx):
         data, target = batch
         preds = self.forward(data)
         
-        loss = self.criterion(transform_to_1d(preds), transform_to_1d(target))
+        loss = self.criterion(transform_to_1d(preds.unsqueeze(1)), transform_to_1d(target.unsqueeze(1)))
         self.log("loss_train", loss, prog_bar = False, logger = True)
         return loss
 
@@ -71,7 +72,7 @@ class ConvNet_regressor(pl.LightningModule):
         data, target = batch
         preds = self.forward(data)
 
-        loss = self.criterion(transform_to_1d(preds), transform_to_1d(target))
+        loss = self.criterion(transform_to_1d(preds.unsqueeze(1)), transform_to_1d(target.unsqueeze(1)))
         self.log("loss_val", loss, prog_bar = False, logger = True)
 
 
@@ -133,36 +134,39 @@ class ConvNet(nn.Module):
         ActivationClass = getattr(nn, activation)
         
         self.real_model = nn.Sequential(
-                    nn.Conv1d(1, 3, kernel_size=2, stride=1, padding=1, bias=bias),
+                    nn.Conv1d(1, 3, kernel_size=3, stride=1, padding=1, bias=bias),
                     nn.BatchNorm1d(3),
                     ActivationClass(),
-                    nn.MaxPool1d(kernel_size=2, stride=2, padding=1),
+                    nn.MaxPool1d(kernel_size=3, stride=1, padding=1),
                     nn.Dropout(0.3),
             
-                    nn.Conv1d(3, 5, kernel_size=2, stride=2, padding=1, bias=bias),
+                    nn.Conv1d(3, 5, kernel_size=3, stride=1, padding=1, bias=bias),
                     nn.BatchNorm1d(5),
                     ActivationClass(),
                     nn.Dropout(0.3),
             
-                    nn.Conv1d(5, 5, kernel_size=2, stride=1, padding=1, bias=bias),
+                    nn.Conv1d(5, 5, kernel_size=3, stride=1, padding=1, bias=bias),
                     nn.BatchNorm1d(5),
                     ActivationClass(),
             
-                    nn.Dropout(0.3),
+                    nn.Dropout(0.3)
                     )
         
+        self.flatten = nn.Flatten()
         self.imag_model = deepcopy(self.real_model)
-        self.real_fc = nn.Linear(5, in_features)
-        self.imag_fc = nn.Linear(5, in_features)
+        self.real_fc = nn.Linear(in_features, in_features)
+        self.imag_fc = nn.Linear(in_features, in_features)
         
         
     def forward(self, real, imag):
         real = self.real_model(real)
-        real = mean(real, dim = 2)
+        real = mean(real, dim = 1)
+        # real = self.flatten(real)
         real = self.real_fc(real)
         
         imag = self.imag_model(imag)
-        imag = mean(imag, dim = 2)
-        imag = self.imag_fc(real)
+        imag = mean(imag, dim = 1)
+        # imag = self.flatten(imag)
+        imag = self.imag_fc(imag)
         
         return real, imag
